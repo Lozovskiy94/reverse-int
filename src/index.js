55a2bf53e4e52cf140d80a39caa05e1b4181a847
Lozovskiy94 <lozovskiy94@gmail.com>
2023-01-12 21:00:57 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let result = []
+    let arr = n.toString().split('')
+    
+        if (arr[0] === '-')
+        {arr =  arr.slice(1)
+            for (i = 0; i < arr.length; i++)
+            {result.unshift(arr[i])}
+            return Number(result.join(''))}
+            
+        else {for (i = 0; i < arr.length; i++)
+            {result.unshift(arr[i])}
+            return +result.join('')}
+    
+    
 }
